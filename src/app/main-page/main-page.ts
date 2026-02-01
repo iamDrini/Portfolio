7fcece7087c917354a380813b946dc4b8d5f8607
iamDrini <valdrin.murselji@outlook.de>
2026-02-01 22:31:10 +0100

    feat- added Font & raw landing component

---
+++ src/app/main-page/main-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LandingPage } from "../landing-page/landing-page";
+
+@Component({
+  selector: 'app-main-page',
+  imports: [LandingPage],
+  templateUrl: './main-page.html',
+  styleUrl: './main-page.scss',
+})
+export class MainPage {
+
+}
