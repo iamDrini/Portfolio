c7580a08248c19dd021ba23d654b673f6c80205d
iamDrini <valdrin.murselji@outlook.de>
2026-02-06 18:04:52 +0100

    feat- portfolio-page: added project screenshots on hover

--- src/app/portfolio-page/portfolio-page.ts
+++ src/app/portfolio-page/portfolio-page.ts
@@ -1,9 +1,10 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-portfolio-page',
-  imports: [TranslocoPipe],
+  imports: [TranslocoPipe, NgStyle],
   templateUrl: './portfolio-page.html',
   styleUrl: './portfolio-page.scss',
 })
