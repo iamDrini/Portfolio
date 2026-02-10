cf9bfbbb1a26de39db2438a59b414ccb40fccd75
iamDrini <valdrin.murselji@outlook.de>
2026-02-10 22:32:33 +0100

    feat- form ready

--- src/app/feedback-page/feedback-data.ts
+++ src/app/feedback-page/feedback-data.ts
@@ -5,12 +5,12 @@ export const FEEDBACK_DATA = {
             text: 'Valdrin setzt Anforderungen sauber und zuverlässig um. Seine Arbeit ist übersichtlich und strukturiert.'
         },
         {
-            name: 'Beispiel - Beispiel',
-            text: 'BeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispiel.'
+            name: 'Dominik - Team Partner',
+            text: 'Valdrin arbeitet sehr strukturiert und liefert Frontend-Lösungen, die nicht nur funktionieren, sondern auch sauber und durchdacht umgesetzt sind. Besonders stark ist sein Blick fürs Detail und die schnelle, zuverlässige Umsetzung im Team.'
         },
         {
-            name: 'Beispiel - Beispiel',
-            text: 'BeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispielBeispiel.'
+            name: 'Timur A. - Ehemaliger Kommolitone',
+            text: 'Während des Studiums war Valdrin ein sehr engagierter und hilfsbereiter Kommolitone. Seine enorme Lernbereitschaft und Detailverliebtheit für Frontendlösungen inspirierte mich in der Programmierung besser zu werden.'
         }
     ],
     en: [
@@ -18,12 +18,13 @@ export const FEEDBACK_DATA = {
             name: 'Sven - Team Partner',
             text: 'Valdrin implements requirements cleanly and reliably. His work is clear and well-stuctured.'
         }, {
-            name: 'Example - Example',
-            text: 'ExampleExampleExampleExampleExampleExampleExampleExampleExampleExampleExampleExampleExampleExample'
+            name: 'Dominik – Team Partner',
+            text: 'Valdrin works in a highly structured manner and delivers frontend solutions that not only function properly but are also implemented cleanly and thoughtfully. His strong attention to detail and his fast, reliable execution within a team stand out in particular.'
         },
         {
-            name: 'Example - Example',
-            text: 'ExampleExampleExampleExampleExampleExampleExampleExampleExampleExampleExampleExampleExampleExample'
+            name: 'Timur A. – Former Fellow Student',
+            text: 'During our studies, Valdrin was a highly engaged and helpful fellow student. His strong willingness to learn and his attention to detail in frontend solutions inspired me to improve my own programming skills.'
         }
+
     ]
 }
