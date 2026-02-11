e3243ab0995c81928ba4d54a6cd01cd43696d67c
iamDrini <valdrin.murselji@outlook.de>
2026-02-11 22:32:54 +0100

    feat- updated privacy policy en & de

--- src/app/shared/legal-notice/legal-notice.ts
+++ src/app/shared/legal-notice/legal-notice.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { TranslocoPipe } from '@jsverse/transloco';
+import { Header } from "../header/header";
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [TranslocoPipe],
+  imports: [TranslocoPipe, Header],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class LegalNotice {
 
--- src/app/shared/privacy-policy/privacy-policy.ts
+++ src/app/shared/privacy-policy/privacy-policy.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { TranslocoPipe } from '@jsverse/transloco';
+import { Header } from "../header/header";
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [TranslocoPipe],
+  imports: [TranslocoPipe, Header],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class PrivacyPolicy {
 
