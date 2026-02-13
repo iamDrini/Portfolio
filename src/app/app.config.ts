d7383366bebdc60668dc136f439e016f7ffd5cb8
iamDrini <valdrin.murselji@outlook.de>
2026-02-13 22:12:14 +0100

    feat-add slide gesture to feedback

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { TranslocoHttpLoader } from './transloco-loader';
 import { provideTransloco } from '@jsverse/transloco';
+import { HammerModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,6 +23,7 @@ export const appConfig: ApplicationConfig = {
         prodMode: false
       },
       loader: TranslocoHttpLoader
-    })
+    }),
+    importProvidersFrom(HammerModule)
   ]
 };
