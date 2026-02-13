1356d9fcb279c1c8beb3fc7bcdc814b09ad19487
iamDrini <valdrin.murselji@outlook.de>
2026-02-13 02:29:03 +0100

    feat - added responsive to  feedback component

--- src/app/feedback/feedback-data.ts
+++ src/app/feedback/feedback-data.ts
@@ -1,28 +1,28 @@
 export const FEEDBACK_DATA = {
     de: [
         {
-            name: 'Sven - Team Partner',
+            name: 'Sven-Team Partner',
             text: 'Valdrin setzt Anforderungen sauber und zuverlässig um. Seine Arbeit ist übersichtlich und strukturiert.'
         },
         {
-            name: 'Dominik - Team Partner',
+            name: 'Dominik-Team Partner',
             text: 'Valdrin arbeitet sehr strukturiert und liefert Frontend-Lösungen, die nicht nur funktionieren, sondern auch sauber und durchdacht umgesetzt sind. Besonders stark ist sein Blick fürs Detail und die schnelle, zuverlässige Umsetzung im Team.'
         },
         {
-            name: 'Timur A. - Ehemaliger Kommolitone',
+            name: 'Timur-Ehem. Kommolitone',
             text: 'Während des Studiums war Valdrin ein sehr engagierter und hilfsbereiter Kommolitone. Seine enorme Lernbereitschaft und Detailverliebtheit für Frontendlösungen inspirierte mich in der Programmierung besser zu werden.'
         }
     ],
     en: [
         {
-            name: 'Sven - Team Partner',
+            name: 'Sven-team partner',
             text: 'Valdrin implements requirements cleanly and reliably. His work is clear and well-stuctured.'
         }, {
-            name: 'Dominik – Team Partner',
+            name: 'Dominik–team partner',
             text: 'Valdrin works in a highly structured manner and delivers frontend solutions that not only function properly but are also implemented cleanly and thoughtfully. His strong attention to detail and his fast, reliable execution within a team stand out in particular.'
         },
         {
-            name: 'Timur A. – Former Fellow Student',
+            name: 'Timur–for. fellow student',
             text: 'During our studies, Valdrin was a highly engaged and helpful fellow student. His strong willingness to learn and his attention to detail in frontend solutions inspired me to improve my own programming skills.'
         }
 
